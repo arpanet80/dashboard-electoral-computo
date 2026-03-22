6a2ddff6aca4c1499317af53945ab8f0756bc423
= <=>
2026-03-22 19:26:15 -0400

    Label

---
+++ maletas-ui/src/app/components/resumen-excel-computo/resumen-excel-computo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resumen-excel-computo',
+  imports: [],
+  templateUrl: './resumen-excel-computo.html',
+  styleUrl: './resumen-excel-computo.css',
+})
+export class ResumenExcelComputo {
+
+}
